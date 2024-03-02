3429190c613274555e375c6223168cb9284a2f2e
Louvenarde <louve@louve.systems>
2024-03-02 11:18:50 +0100

    Commit

--- Assets/Scripts/BlahaijuController.cs
+++ Assets/Scripts/BlahaijuController.cs
@@ -1,3 +1,4 @@
+using LouveSystems.LagOps;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -6,14 +7,87 @@ using UnityEngine;
 
 public class BlahaijuController : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0f, 10000f)]
+    public float forceAmount = 2000f;
+
+    [SerializeField]
+    private AnimationCurve precisionOverDistance = AnimationCurve.Constant(0f, 1f, 1F);
+
+    [SerializeField]
+    private AnimationCurve forceMultiplierOverDistance = AnimationCurve.Constant(0f, 1f, 1F);
+
+    [SerializeField]
+    private CollisionEventTransmitter collisionEventTransmitter;
+
+    //[SerializeField]
+    //private AnimationCurve visualCatchUpMultiplierWithDistance = AnimationCurve.Linear(0f, 0.2f, 10f, 1F);
+
+    //[SerializeField]
+    //private Transform visualBody;
+
+    //[SerializeField]
+    //[Range(0f, 1000f)]
+    //private float visualLatency = 0.2f;
+
     public Rigidbody body;
-    public Transform head;
-    public float forceAmount;
 
+    private float precisionMultiplier = 0f;
+    private float forceMultiplier = 1f;
+    private Vector3 destination;
 
-    void Update()
+    private void Awake()
     {
-        //Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - head.position;
-        //body.AddForceAtPosition(forceAmount, head, ForceMode.Acceleration);
+        collisionEventTransmitter.onColliderEnter += CollisionEventTransmitter_onColliderEnter;
+    }
+
+    private void CollisionEventTransmitter_onColliderEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log($"Blahaj hit {other}");
+        }
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            destination = hit.point;
+            float mag = Vector3.Magnitude(destination - transform.position);
+            precisionMultiplier = precisionOverDistance.Evaluate(mag);
+            forceMultiplier = forceMultiplierOverDistance.Evaluate(mag);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Quaternion previousVisualRotation = visualBody.rotation;
+
+        Vector3 directionVector = destination - transform.position;
+
+        Vector3 perfectDirection = directionVector.normalized;
+        Vector3 perfectDirectionHorizontal = (destination - transform.position).WithoutY().normalized;
+
+        Vector3 actualDirection = Vector3.Lerp(transform.forward, perfectDirection, precisionMultiplier);
+        Vector3 actualDirectionHorizontal = Vector3.Lerp(transform.forward, perfectDirectionHorizontal, precisionMultiplier);
+        
+        body.AddForce(
+            actualDirection * 
+            forceAmount *
+            forceMultiplier *
+            Time.fixedDeltaTime
+        );
+
+        body.transform.LookAt(body.transform.position + actualDirectionHorizontal, Vector3.up);
+
+        //float visualCatchUpMultiplier = visualCatchUpMultiplierWithDistance.Evaluate(directionVector.magnitude);
+
+        //visualBody.rotation = Quaternion.Lerp(
+        //    previousVisualRotation, 
+        //    visualBody.rotation, 
+        //    visualLatency * Time.fixedDeltaTime * visualCatchUpMultiplier
+        //);
     }
 }
