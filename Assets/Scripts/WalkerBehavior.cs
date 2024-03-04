05192ccf93560e9bf73e2e9151b08f223cece11a
Roxanne <roxanne@thegamebakers.com>
2024-03-04 11:02:01 +0100

    Signs

--- Assets/Scripts/WalkerBehavior.cs
+++ Assets/Scripts/WalkerBehavior.cs
@@ -15,14 +15,37 @@ public class WalkerBehavior : EnemyBehavior
     [SerializeField]
     private Animator animator;
 
+    [SerializeField]
+    private float lookAtSpeed = 2f;
+
+    private Transform signTransform;
+
     public void GiveSign()
     {
-        animator.SetBool("HasSign", true);
-        GameObject sign = possibleSigns[Random.Range(0, possibleSigns.Length)];
-       
-        var inst =Instantiate(sign, signParent);
-        inst.transform.localPosition = Vector3.zero;
-        inst.transform.localRotation= Quaternion.identity;
-        inst.transform.localScale = Vector3.one;
+        if (signParent)
+        {
+            animator.SetBool("HasSign", true);
+            GameObject sign = possibleSigns[Random.Range(0, possibleSigns.Length)];
+
+            var inst = Instantiate(sign, signParent);
+            inst.transform.localPosition = Vector3.zero;
+            inst.transform.localRotation = Quaternion.identity;
+            inst.transform.localScale = Vector3.one;
+
+            signTransform = inst.transform;
+            //inst.transform.LookAt(Camera.main.transform);
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (signTransform)
+        {
+            Quaternion look = Quaternion.LookRotation(Camera.main.transform.position - signTransform.position);
+            signTransform.rotation = look;
+            //signTransform.rotation = Quaternion.Lerp(signTransform.rotation, look, Time.deltaTime * lookAtSpeed);
+        }
     }
 }
